97b96e92622e0ede0ce21530b57aecddf556b789
biancanev <bebostunner234@gmail.com>
2023-12-30 10:55:01 -0800

    refactored backend.py

--- system/backend.py
+++ system/backend.py
@@ -1,4 +1,16 @@
 import math
+import pymongo as pm
+
+client = pm.MongoClient("mongodb://localhost:27017")
+itemdb = client["itemdb"]
+items = itemdb["Items"]
+
+#test_item = {"id": 123456, "upc": 123456789012, "price": 12.34, "name": "test"}
+
+#items.insert_one(test_item)
+#query = {"name": "test"}
+#results = items.find_one(query)
+#print(results["id"])
 
 class Item:
     def __init__(self):
@@ -6,34 +18,55 @@ class Item:
         self.upc = int()
         self.price = int()
         self.name = str()
-    def findItemById(id):
-        #TODO: connect to Mongodb to find item by id
-        pass
+    def findItemById(self, id):
+        query = {"id": id}
+        item = items.find_one(query)
+        if item is not None:
+            self.id = item["id"]
+            self.upc = item["upc"]
+            self.price = item["price"]
+            self.name = item["name"]
+        else:
+            self.id = -1
+        return
     def findItemByUPC(self, upc):
-        #TODO: connect to Mongodb to find item by upc
-        return self
+        query = {"upc": upc}
+        item = items.find_one(query)
+        if item is not None:
+            self.id = item["id"]
+            self.upc = item["upc"]
+            self.price = item["price"]
+            self.name = item["name"]
+        else:
+            self.id = -1
+        return
     def findItemByName(self, name):
         #TODO: connect to Mongodb to find item by name
         pass
-    def isValidItem():
-        return False if id == -1 else True
+    def isValidItem(self):
+        return False if self.id == -1 else True
 
 class Cart:
     def __init__(self):
         self.subtotal = int()
         self.tax = int()
         self.total = int()
-        self.items = dict()
+        self.items = []
     def addItemToCart(self, num):
         newItem = Item()
-        if math.log10(num) + 1 == 12:
+        if len(str(num)) == 12:
             newItem.findItemByUPC(num)
-            if self.items[newItem] == None:
-                self.items[newItem] = 1
-            else:
-                self.items[newItem] += 1
+            self.items.append(newItem) if newItem.isValidItem() else print("Invalid UPC")
+        else:
+            newItem.findItemById(num)
+            self.items.append(newItem) if newItem.isValidItem() else print("Invalid ID")
+    def displayCart(self):
+        for item in self.items:
+            print(item.id, item.name, ":", item.price)
             
-
 cart = Cart()
+cart.addItemToCart(123456)
 cart.addItemToCart(123456789012)
-print(cart.items)
+cart.addItemToCart(111111)
+cart.addItemToCart(123456789000)
+cart.displayCart()
