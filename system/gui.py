4cf98af4d847904923cf461caeb691a78e08273f
Alec Fong <fongalec@gmail.com>
2023-12-30 23:21:29 -0800

    made the cart display left-justified

--- system/gui.py
+++ system/gui.py
@@ -7,7 +7,7 @@ frame = ttk.Frame(root, padding = 10)
 frame.grid()
 title = Label(frame, text = "Best Buy", fg = "blue").grid(column = 0, row = 0)
 cart = backend.Cart()
-dispCart = Label(frame, text = cart.displayCart()).grid(column = 0, row = 1)
+dispCart = Label(frame, text = cart.displayCart(), justify = "left").grid(column = 0, row = 1)
 quit = Button(frame, text = "Quit", command = root.destroy).grid(column = 1000, row = 1000)
 
 root.mainloop()
