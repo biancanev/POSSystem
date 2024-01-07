dbaca0ae3c316aabde954b8e2c03a5a248359476
Alec Fong <fongalec@gmail.com>
2024-01-06 19:53:15 -0800

    a start on the GUI

--- system/gui.py
+++ system/gui.py
@@ -1,13 +1,49 @@
-from tkinter import *
+import tkinter as tk
 from tkinter import ttk
+from tkinter import *
+from PIL import ImageTk, Image
 # import backend
 
-root = Tk()
-frame = ttk.Frame(root, padding = 10)
-frame.grid()
-title = Label(frame, text = "Best Buy", fg = "blue", font = {"futura", 36, "bold italic"}).grid(column = 0, row = 0)
-# cart = backend.Cart()
-# dispCart = Label(frame, text = cart.displayCart(), justify = "left").grid(column = 0, row = 1)
-quit = Button(frame, text = "Quit", command = root.destroy).grid(column = 1000, row = 1000)
+window  = tk.Tk()
+window.title("Best Buy POS System")
+window.rowconfigure(0, minsize = 800, weight = 1)
+window.columnconfigure(1, minsize = 800, weight = 1)
 
-root.mainloop()
+image1 = Image.open("C:/Users/fonga/Downloads/POSSystem/POSSystem/system/bestbuy.jpg")
+photo = ImageTk.PhotoImage(image1)
+
+frm_sidebar = tk.Frame(window, relief = tk.RAISED, borderwidth = 2)
+lbl_title = tk.Label(
+    frm_sidebar,
+    image = photo
+)
+
+book_options = ttk.Notebook()
+txt_cartDisplay = tk.Text(book_options)
+txt_cartDisplay.insert("1.0", "Cart:")
+lbl_option1 = tk.Label(text = "")
+lbl_option2 = tk.Label(text = "")
+book_options.add(
+    txt_cartDisplay,
+    state = "normal",
+    sticky = "nsew",
+    padding = 2,
+    text = "Cart"
+)
+book_options.add(
+    lbl_option2,
+    state = "normal",
+    sticky = "nsew",
+    padding = 2,
+    text = "Checkout"
+)
+
+# grid
+lbl_title.grid(row = 0, column = 0, sticky = "ew")
+frm_sidebar.grid(row = 0, column = 0, sticky = "ns")
+book_options.grid(row = 0, column = 1, sticky = "nsew")
+
+# cart = backend.Cart()
+# cartDisplay.insert(0, cart.displayCart())
+
+window.mainloop()
