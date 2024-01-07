3df200118e45287e3652e471aae574a7f4fd6c16
Alec Fong <fongalec@gmail.com>
2024-01-06 21:30:40 -0800

    added some things

--- system/gui.py
+++ system/gui.py
@@ -2,29 +2,49 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import *
 from PIL import ImageTk, Image
+from pathlib import Path
 import backend
 
-root = Tk()
+root = tk.Tk()
 root.attributes('-fullscreen', True)
 root.option_add("*Font", 'Verdana 20')
-frame = ttk.Frame(root, padding = 10)
-frame.grid()
-#Menubar
+root.rowconfigure(10, minsize = 800, weight = 1)
+root.columnconfigure(10, minsize = 800, weight = 1)
+logoFrame = tk.Frame(root, relief = tk.RIDGE, borderwidth = 7)
+dispFrame = tk.Frame(root, relief = tk.GROOVE, borderwidth = 5)
+quitFrame = tk.Frame(root)
+
+# Menu bar
 menu = Menu(root)
-root.config(menu=menu)
-#Transaction Dropdown
+root.config(menu = menu)
+
+# Transaction dropdown
 transaction = Menu(root)
-transaction.add_command(label="Void Transaction", command=backend.Order().voidOrder())
-menu.add_cascade(label="Transaction", menu=transaction, font="Verdana 20")
-#Customer dropdown
+transaction.add_command(label = "Void Transaction", command = backend.Order().voidOrder())
+menu.add_cascade(label = "Transaction", menu = transaction, font = "Verdana 20")
+
+# Customer dropdown
 customer = Menu(menu)
-customer.add_command(label="Search Customer")
-menu.add_cascade(label="Customer", menu=customer)
-#Main window text
-title = Label(frame, text = "Best Buy", fg = "blue").grid(column = 0, row = 0)
+customer.add_command(label = "Search Customer")
+menu.add_cascade(label = "Customer", menu = customer)
+
+# Main window text
+root_dir = Path(__file__).resolve().parent.parent
+fileName = root_dir / "system" / "bestbuy.jpg"
+image1 = Image.open(fileName)
+photo = ImageTk.PhotoImage(image1)
+title = Label(logoFrame, image = photo)
 cart = backend.Cart()
-dispCart = Label(frame, text = cart.displayCart(), justify = "left").grid(column = 0, row = 1)
-quit = Button(frame, text = "Quit", command = root.destroy).grid(column = 1000, row = 1000)
+dispCart = Label(dispFrame, text = cart.displayCart(), justify = "left")
+quit = Button(quitFrame, text = "Quit", command = root.destroy)
+
+# Grid
+title.grid(row = 0, column = 0)
+logoFrame.grid(row = 0, column = 0)
+dispCart.grid(row = 1, column = 0)
+dispFrame.grid(row = 1, column = 0, sticky = "w")
+quit.grid(row = 2, column = 0)
+quitFrame.grid(row = 2, column = 0, sticky = "w")
 
 root.mainloop()
 
