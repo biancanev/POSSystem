1bac4f9b5ad5f7b6a35672fb3a8ec606d0f09f6a
biancanev <bebostunner234@gmail.com>
2024-01-12 20:24:55 -0800

    Added more updates

--- system/payment.py
+++ system/payment.py
@@ -4,9 +4,6 @@ from decimal import *
 from dotenv import load_dotenv
 import os
 
-import collections
-collections.MutableSequence = collections.abc.MutableSequence
-
 load_dotenv()
 
 merchantAuth = apicontractsv1.merchantAuthenticationType()
@@ -15,7 +12,27 @@ merchantAuth.transactionKey = os.getenv("CREDIT_CARD_KEY")
 
 creditCard = apicontractsv1.creditCardType()
 creditCard.cardNumber = "4111111111111111"
-creditCard.expirationDate = "2024-12"
+creditCard.expirationDate = "2020-12"
  
 payment = apicontractsv1.paymentType()
 payment.creditCard = creditCard
+transactionrequest = apicontractsv1.transactionRequestType()
+transactionrequest.transactionType ="authCaptureTransaction"
+transactionrequest.amount = Decimal ('1.55')
+transactionrequest.payment = payment
+ 
+ 
+createtransactionrequest = apicontractsv1.createTransactionRequest()
+createtransactionrequest.merchantAuthentication = merchantAuth
+createtransactionrequest.refId ="MerchantID-0001"
+ 
+createtransactionrequest.transactionRequest = transactionrequest
+createtransactioncontroller = createTransactionController(createtransactionrequest)
+createtransactioncontroller.execute()
+ 
+response = createtransactioncontroller.getresponse()
+ 
+if (response.messages.resultCode=="Ok"):
+       print("Transaction ID : %s"% response.transactionResponse.transId)
+else:
+       print("response code: %s"% response.messages.resultCode)
